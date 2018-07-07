e8ab4834030591a28aee1b5944cb193626554c70
lujin <jinlu@epiclouds.net>
2018-07-07 10:26:36 +0800

    一个生成manifest的插件

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # html-webpack-manifest-plugin
-自动生成一个manifest的`html-webpack-plugin`插件
+
+自动生成一个`manifest`的`html-webpack-plugin`插件
+
+同步将`manifest`文件注入到`html`中，需要配合`html-webpack-plugin`一起使用
+
+插件会同时生成需要尺寸的`favicon`
+
+**这个插件我是在用了好几个自动生成 favicon 的插件之后非常不满意，比如[这个](https://github.com/itgalaxy/favicons),这么多 star，但是连基本的 manifest 需要的字段都能丢了，pr 也不合并，只能自己来了**
+
+## 使用
+
+没有上传 `npm`，所以直接`github`安装
+
+这个库目前主要依赖了`sharp`和`lodash`两个库，依赖没有装成功就手动安装咯...我搞不清这些东西怎么自动安装的，垃圾`npm`
+
+```js
+module.exports = {
+    ...
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new HtmlWebpackManifestPlugin({
+            manifest: {
+                // manifest文件名称，带有 [hash:8]可以指定hash值和hash长度
+                filename: 'manifest.[hash:8].json',
+                // manifest文件的位置
+                filePath: '/static/',
+                // manifest文件前缀，主要还是自动添加cdn之类的作用
+                prefix: 'https://cdn.com/'
+                // manifest文件中的icon,指定图标的位置，大小尺寸之后会自动生成对应的图标
+                icons: {
+                    // 图标位置
+                    logo: path.resolve(__dirname, './static/logo.png'),
+                    // 图标尺寸，都是1:1的图
+                    sizes: [36, 48, 72],
+                    // 图标类型
+                    type: 'image/png',
+                    // 输出文件的URL
+                    output: '/static/icons-[hash:8]/',
+                    // 增加文件前缀，可以添加cdn地址
+                    prefix: '/'
+                }
+                config: {
+                    // 这里可以接受manifest文件中需要的任何参数，也就是你填什么在这，生成的manifest文件中就有什么
+                    name: 'html webpack manifest plugin',
+                    short_name: 'manifest',
+                    ...
+                }
+            },
+            // 这里主要配置生成head中的一些链接，按照规则自动生成并inject
+            linkGroup: [{
+                attr: 'href',
+                attributes: {
+                    rel: 'dns-prefetch',
+                },
+                links: [
+                    'https://cdn.com/'
+                ]
+            }, {
+                attr: 'href',
+                attributes: {
+                    rel: 'preconnect'
+                },
+                links: [
+                    'https://cdn.com/',
+                ]
+            }]
+        })
+    ]
+}
+```
--- index.js
+++ index.js
@@ -1,11 +1,220 @@
-import _ from 'lodash';
+const _ = require('lodash')
+const path = require('path')
+const url = require('url')
+const crypto = require('crypto')
+const sharp = require('sharp')
 
 
-class HtmlWebpackManifestPlugin {
-  constructor(options) {
-    this.options = _.extend({
+const PLUGIN_NAME = 'html-webpack-manifest-plugin'
 
-    }, options)
+function hexHash(hashContent) {
+  const hashObj = crypto.createHash('md5').update(hashContent)
+  return hashObj.digest('hex')
+}
 
+function extraHashName(hashPath) {
+  const m = hashPath.match(/\[(\w+):?(\d*)\]/)
+  if (m) {
+    return [m[1], m[2]]
+  } else {
+    return null
   }
 }
+
+function genManifest(manifest, compilation, iconFiles) {
+  let fileName = manifest.filename
+  const filePath = manifest.filePath
+
+  const manifestContent = manifest.config
+  if (iconFiles) {
+    manifestContent.icons = iconFiles
+  }
+  const manifestJson = JSON.stringify(manifestContent)
+  // 需要计算hash,直接md5即可
+  // 抽取文件名中hash
+  const m = fileName.match(/\[(\w+):?(\d*)\]/)
+  if (m) {
+    const hashType = m[1]
+    const hashLength = m[2]
+    const hashObj = crypto.createHash('md5').update(manifestJson)
+    const fullHash = hashObj.digest('hex')
+    console.log('fullHash ==>', fullHash)
+    let fileHash = fullHash
+    if (hashLength) {
+      fileHash = fullHash.substr(0, hashLength)
+    }
+    fileName = fileName.replace(/\[.*\]/, fileHash)
+    console.log('\nfileName ==>', fileName)
+  }
+  let outputPath = path.resolve(filePath, fileName)
+  compilation.assets[outputPath] = {
+    source: function () {
+      return manifestJson
+    },
+    size: function () {
+      return manifestJson.length
+    },
+    type: 'manifest'
+  }
+}
+
+function HtmlWebpackManifestPlugin(options) {
+  this.options = _.merge({}, {
+    manifest: {
+      filename: 'manifest.[hash:8].json',
+      filePath: '/static/',
+      prefix: '/',
+      config: {
+        name: 'html webpack manifest plugin',
+        short_name: 'manifest',
+        start_url: '/?homescreen=1',
+        display: "standalone",
+        background_color: '#fff',
+        description: null,
+        icons: [],
+        lang: 'en-US',
+        orientation: 'portrait',
+        scope: '/',
+        theme_color: '#fff'
+      }
+    }
+  }, options)
+  console.log(this.options)
+  this.beforeV4 = false
+}
+
+
+HtmlWebpackManifestPlugin.prototype.apply = function (compiler) {
+  this.beforeV4 = !compiler.hooks
+  const that = this
+  if (!this.beforeV4) {
+
+    compiler.hooks['compilation'].tap(PLUGIN_NAME, function (compilation) {
+      compilation.hooks.htmlWebpackPluginBeforeHtmlGeneration
+        .tapAsync(PLUGIN_NAME, function (htmlPluginData, cb) {
+          // 生成一个manifest
+          const manifest = that.options.manifest
+          console.log('manifest =>', manifest)
+          if (manifest) {
+            // 生成favicons
+            const icons = manifest.icons
+            const logoPath = icons.logo
+
+            const promises = [sharp(logoPath).toBuffer()].concat(icons.sizes.map(function (size) {
+              return sharp(logoPath).resize(size).toBuffer()
+            }))
+            Promise.all(promises)
+              .then(function (results) {
+                const oldResult = results[0]
+                const fullHash = hexHash(oldResult)
+                const hashArrs = extraHashName(icons.output)
+                let output = icons.output
+                if (hashArrs) {
+                  let fileHash = fullHash
+                  if (hashArrs[1]) {
+                    fileHash = fullHash.substr(0, hashArrs[1])
+                  }
+                  output = output.replace(/\[.*\]/, fileHash)
+                }
+
+                let iconFiles = []
+                results.slice(1).map(function (data, index) {
+                  const size = icons.sizes[index]
+                  const outputPath = path.resolve(output, `android-chrome-${size}x${size}.png`)
+                  const prefix = icons.prefix
+                  iconFiles.push({
+                    "src": prefix ? url.resolve(prefix, outputPath) : outputPath,
+                    "sizes": `${size}x${size}`,
+                    "type": icons.type
+                  })
+                  compilation.assets[outputPath] = {
+                    source: function () {
+                      return data
+                    },
+                    size: function () {
+                      return data.length
+                    },
+                    type: 'icon'
+                  }
+                })
+
+                genManifest(manifest, compilation, iconFiles)
+
+                cb(null, htmlPluginData)
+              })
+          } else {
+            cb(null, htmlPluginData)
+          }
+        })
+
+      compilation.hooks.htmlWebpackPluginAlterAssetTags
+        .tapAsync(PLUGIN_NAME, function (htmlPluginData, cb) {
+          // gen link
+          const linkGroup = that.options.linkGroup
+          if (linkGroup) {
+            linkGroup.map(function (group) {
+              const groupAttrs = group.attributes
+              const attr = group.attr || 'href'
+              const groupTags = group.links.map(function (link) {
+                let attributes = {}
+                if (typeof link === 'string') {
+                  attributes[attr] = link
+                  attributes = _.merge({}, groupAttrs, attributes)
+                } else {
+                  attributes = _.merge({}, groupAttrs, link)
+                }
+                return {
+                  tagName: 'link',
+                  closeTag: false,
+                  attributes: attributes
+                }
+                return tag
+              })
+              htmlPluginData.head = htmlPluginData.head.concat(groupTags)
+            })
+          }
+
+          // inject manifest file
+          const assets = compilation.assets
+          if (assets) {
+            Object.keys(assets)
+              .map(function (key) {
+                const asset = assets[key]
+                if (asset.type === 'manifest') {
+                  const prefix = that.options.manifest.prefix
+                  const href = prefix ? url.resolve(prefix, key) : key
+                  htmlPluginData.head.push({
+                    tagName: 'link',
+                    closeTag: false,
+                    attributes: {
+                      rel: asset.type,
+                      href: href
+                    }
+                  })
+                }
+                // else if (asset.type === 'icon') {
+                //   const prefix = that.options.manifest.icons.prefix
+                //   const href = prefix ? url.resolve(prefix, key) : key
+                //   htmlPluginData.head.push({
+                //     tagName: 'link',
+                //     closeTag: false,
+                //     attributes: {
+                //       rel: asset.type,
+                //       href: href
+                //     }
+                //   })
+                // }
+              })
+          }
+          cb(null, htmlPluginData)
+        })
+    })
+
+    // compiler.hooks.afterEmit.tapAsync(PLUGIN_NAME, function (compilation) {
+    //   console.log('afterEmit compilation ==>', compilation.assets)
+    // })
+  }
+}
+
+
+module.exports = HtmlWebpackManifestPlugin
